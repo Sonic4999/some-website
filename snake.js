a048aee1de1bf4b67dcd19ee06d7a8c30b24b93a
Sonic4999 <sonictbm@gmail.com>
2018-03-22 13:01:50 -0400

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,5 +1,8 @@
-var snake = [{ top: 0, left: 0}]
+var drawSnake = function(snakeToDraw) {
+  var drawableSnake = { color: "green", pixels: snakeToDraw };
+  var drawableObjects = [drawableSnake];
+  CHUNK.draw(drawableObjects);
+}
 
-var drawableSnake = { color: "green", pixels: snake };
-var drawableObjects = [drawableSnake];
-CHUNK.draw(drawableObjects);
+var snake = [{ top: 0, left: 0}];
+drawSnake(snake);
